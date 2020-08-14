30b74d541840f4918d7f2d795533b1bdd11fd489
andacas <andacast@espol.edu.ec>
2020-08-13 22:25:21 -0500

    toda la funcionalidad del proyecto validada

--- app/src/main/java/com/example/amst2/Categorias.java
+++ app/src/main/java/com/example/amst2/Categorias.java
@@ -1,87 +1,75 @@
 package com.example.amst2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.solver.widgets.WidgetContainer;
 
-import android.app.Activity;
-import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-import static com.example.amst2.BuildConfig.DEBUG;
-
-public class ventana_principal extends AppCompatActivity {
-
+public class Categorias extends AppCompatActivity {
     private  String consulta_lista = "https://tareaautonoma4.000webhostapp.com/lista_imagenes.php";
     private String consulta_libros = "https://tareaautonoma4.000webhostapp.com/lista_libros.php";
+    private  String consultar_generos = "https://tareaautonoma4.000webhostapp.com/lista_generos.php";
     private  String info_usuario = "";
     Map<String,String> diccionario_lista = new HashMap<String,String>();
-
+    Map<String,String> diccionario_genero = new HashMap<String,String>();
     ArrayList<libro> lista_libro_por_genero = new ArrayList<>();
+    ArrayList<libro> momentaneo = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ventana_principal);
+        setContentView(R.layout.activity_categorias);
         Bundle bundle = getIntent().getExtras();
         String text = bundle.getString("direccion");
-        info_usuario = text;
 
+        info_usuario = text;
         consultar_lista_imagenes(consulta_lista);
         crear_diccionarios_de_libros(consulta_libros);
+        crear_dicionario_generos(consultar_generos);
         init();
     }
 
-public void consultar_lista_imagenes(String url){
-    String[] resultado = null;
-    try {
-        String[] datos = new String[]{
-                "call",
-                url
-        };
-        AsyncImageQuery async = new AsyncImageQuery();
-        resultado = async.execute(datos).get();
+    public void consultar_lista_imagenes(String url){
+        String[] resultado = null;
+        try {
+            String[] datos = new String[]{
+                    "call",
+                    url
+            };
+            AsyncImageQuery async = new AsyncImageQuery();
+            resultado = async.execute(datos).get();
 
 
-        String[] arr_tex = resultado[0].split("///");
-        for(String lineas:arr_tex){
-            String[] valores = lineas.split("~");
-            diccionario_lista.put(valores[0],valores[1]);
+            String[] arr_tex = resultado[0].split("///");
+            for(String lineas:arr_tex){
+                String[] valores = lineas.split("~");
+                diccionario_lista.put(valores[0],valores[1]);
+            }
+
+
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-
-
-    } catch (ExecutionException e) {
-        e.printStackTrace();
-    } catch (InterruptedException e) {
-        e.printStackTrace();
     }
-}
     public void crear_diccionarios_de_libros(String url ){
         String[] resultado = null;
         try {
@@ -105,6 +93,46 @@ public void consultar_lista_imagenes(String url){
     }
     public void init() {
         TableLayout stk = (TableLayout) findViewById(R.id.table_main);
+        for (final String i :diccionario_genero.keySet()) {
+            TableRow tbrow = new TableRow(this);
+            Button t1v = new Button(this);
+            t1v.setText(diccionario_genero.get(i));
+
+            t1v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    por_categoria(diccionario_genero.get(i));
+                }
+
+
+            });
+
+
+            tbrow.addView(t1v);
+            stk.addView(tbrow);
+        }
+
+    }
+    public void informacion_personal(View view){
+        Intent i = new Intent(this, Informacion.class );
+        i.putExtra("direccion", info_usuario);
+        startActivity(i);
+    }
+    public void por_categoria(String cat){
+       momentaneo =  new ArrayList();
+       for(libro i : lista_libro_por_genero){
+           if(diccionario_genero.get(i.genero) == cat){
+               momentaneo.add(i);
+           }
+       }
+       lista_libro_por_genero = momentaneo;
+       init2();
+    }
+
+    public void init2() {
+        TableLayout stk = (TableLayout) findViewById(R.id.table_main);
+        stk.removeAllViews();
         TableRow tbrow0 = new TableRow(this);
         TextView tv0 = new TextView(this);
         tv0.setText(" Portada ");
@@ -156,7 +184,10 @@ public void consultar_lista_imagenes(String url){
                     addContentView(anotherLayout, linearLayoutParams);
                 }
 
-
+                public String get_libro(){
+                    String lib = i.autor;
+                    return lib;
+                }
             });
 
 
@@ -170,17 +201,41 @@ public void consultar_lista_imagenes(String url){
         }
 
     }
-    public void informacion_personal(View view){
-        Intent i = new Intent(this, Informacion.class );
-        i.putExtra("direccion", info_usuario);
-        startActivity(i);
+    public void crear_dicionario_generos(String url){
+        String[] resultado = null;
+        try {
+            String[] datos = new String[]{
+                    "call",
+                    url
+            };
+            AsyncImageQuery async = new AsyncImageQuery();
+            resultado = async.execute(datos).get();
+
+
+            String[] arr_tex = resultado[0].split("///");
+            for(String lineas:arr_tex){
+                String[] valores = lineas.split("~");
+                diccionario_genero.put(valores[0],valores[1]);
+            }
+
+
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-    public void categorias(View view){
+    public void menu_categorias(View view){
         Intent i = new Intent(this, Categorias.class );
         i.putExtra("direccion", info_usuario);
         startActivity(i);
 
     }
+    public void menu_principal(View view){
+        Intent i = new Intent(this, ventana_principal.class );
+        i.putExtra("direccion", info_usuario);
+        startActivity(i);
 
-
+    }
 }
--- app/src/main/java/com/example/amst2/Informacion.java
+++ app/src/main/java/com/example/amst2/Informacion.java
@@ -7,9 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
 public class Informacion extends AppCompatActivity {
     private  String info_usuario;
     TextView nombre,apellido,correo,celular,genero,id_persona;
+    private String consulta_generos = "https://tareaautonoma4.000webhostapp.com/lista_generos.php";
+    Map<String,String> diccionario_genero = new HashMap<String,String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +30,47 @@ public class Informacion extends AppCompatActivity {
         celular = (TextView) findViewById(R.id.txt_celular);
         genero = (TextView) findViewById(R.id.txt_genero);
         id_persona = (TextView) findViewById(R.id.txt_id);
+        crear_dicionario_generos(consulta_generos);
         id_persona.setText(arr_text[0]);
         nombre.setText(arr_text[1]);
         apellido.setText(arr_text[2]);
         correo.setText(arr_text[3]);
         celular.setText(arr_text[4]);
-        genero.setText(arr_text[5]);
+        genero.setText(diccionario_genero.get(arr_text[5]));
     }
     public void menu_principal(View view){
         Intent i = new Intent(this, ventana_principal.class );
+        i.putExtra("direccion", info_usuario);
+        startActivity(i);
+
+    }
+    public void crear_dicionario_generos(String url){
+        String[] resultado = null;
+        try {
+            String[] datos = new String[]{
+                    "call",
+                    url
+            };
+            AsyncImageQuery async = new AsyncImageQuery();
+            resultado = async.execute(datos).get();
+
+
+            String[] arr_tex = resultado[0].split("///");
+            for(String lineas:arr_tex){
+                String[] valores = lineas.split("~");
+                diccionario_genero.put(valores[0],valores[1]);
+            }
+
+
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void menu_categorias(View view){
+        Intent i = new Intent(this, Categorias.class );
         i.putExtra("direccion", info_usuario);
         startActivity(i);
 
